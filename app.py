7e17e907ccd98c6111a1bb7dbc6df2bb4683b8f7
Felix Augenstein <50483247+FelixAugenstein@users.noreply.github.com>
2021-03-29 19:32:10 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, render_template, json, url_for
+from decouple import config
 
 app = Flask(__name__, template_folder='template')
+
+# Local env variables
+API_KEY = config('apikey')
+URL = config('url')
+SPACE_ID = config('space_id')
+DEPLOYMENT_ID = config('deployment_id')
 
 @app.route('/')
 def index():
@@ -28,38 +35,23 @@ def my_form_post():
     total_intl_charge = request.form['total_intl_charge']
     customer_service_calls = request.form['customer_service_calls']
 
-    # -------------------------------------------------------------
-    # YOUR INPUT REQUIRED HERE
-    # -------------------------------------------------------------
-    # #############################################################
-    # -------------------------------------------------------------
-    # PLEASE PROVIDE YOUR API-KEY, URL, SPACE ID AND DEPLOYMENT ID:
-    # -------------------------------------------------------------
-
-    # PROVIDE YOUR API-KEY & URL HERE:
     # wml authentication
     from ibm_watson_machine_learning import APIClient
 
     wml_credentials = {
-                    "url": "YOUR-URL-HERE", # the default url for wml services in the US is https://us-south.ml.cloud.ibm.com
-                    "apikey":"YOUR-API-KEY-HERE" 
+                    "url": URL, # the default url for wml services in the US is https://us-south.ml.cloud.ibm.com
+                    "apikey": API_KEY
                     }
 
     client = APIClient(wml_credentials)
     print(client.version)
 
-    # PROVIDE YOUR SPACE ID HERE:
     # deployment space
-    space_id = "YOUR-SPACE-ID-HERE"
+    space_id = SPACE_ID
     client.set.default_space(space_id)
 
-    # PROVIDE YOUR DEPLOYMENT ID HERE:
     # deployment id
-    deployment_id = 'YOUR-DEPLOYMENT-ID-HERE'
-
-    # -------------------------------------------------------------
-    # END: PLEASE PROVIDE YOUR API-KEY, URL, SPACE ID AND DEPLOYMENT ID
-    # -------------------------------------------------------------
+    deployment_id = DEPLOYMENT_ID
 
     # test deployment with test data
     scoring_data = {
@@ -74,31 +66,26 @@ def my_form_post():
     predictions = client.deployments.score(deployment_id, scoring_data)
     print("The Prediction output regarding customer churn will be displayed in this format 1 for True or 0 for False: \n ", predictions)
 
-    # prediction values
-    prediction_churn_true_or_false = predictions['predictions'][0]['values'][0][0]
-    prediction_churn_true_or_false_percentage_one = predictions['predictions'][0]['values'][0][1][0]
-    prediction_churn_true_or_false_percentage_two = predictions['predictions'][0]['values'][0][1][1]
+    # import values.py
+    from prediction import values
+    values.predictionValues(predictions)
 
-    # prediction rules for customer churn true or false
-    if prediction_churn_true_or_false == 0:
-        churn_result = "This customer will not churn."
-    elif prediction_churn_true_or_false == 1:
-        churn_result = "This customer will churn."
-    else:
-        churn_result = "There is no accurate prediction for this customer." 
+    # obtain variables from values.py
+    prediction_churn_true_or_false = values.predictionValues.prediction_churn_true_or_false
+    prediction_churn_true_or_false_percentage_one = values.predictionValues.prediction_churn_true_or_false_percentage_one
+    prediction_churn_true_or_false_percentage_two = values.predictionValues.prediction_churn_true_or_false_percentage_two
+
+    # import rules.py
+    from prediction import rules
+    rules.predictionRules(prediction_churn_true_or_false, prediction_churn_true_or_false_percentage_one, prediction_churn_true_or_false_percentage_two)
+    
+    # obtain variables from rules.py
+    churn_result = rules.predictionRules.churn_result
     print(churn_result)
-
-    # prediction rules for confidence score
-    if prediction_churn_true_or_false_percentage_one > prediction_churn_true_or_false_percentage_two:
-        churn_result_percentage = "The confidence score is: " +str(prediction_churn_true_or_false_percentage_one * 100)+"%"
-    elif prediction_churn_true_or_false_percentage_one < prediction_churn_true_or_false_percentage_two:
-        churn_result_percentage = "The confidence score is: " +str(prediction_churn_true_or_false_percentage_two * 100)+"%"
-    else:
-        churn_result_percentage = "There is no accurate confidence score for this prediction."    
+    churn_result_percentage = rules.predictionRules.churn_result_percentage
     print(churn_result_percentage)
 
     return render_template('result.html', state=state, account_length=account_length, area_code=area_code, international_plan=international_plan, voice_mail_plan=voice_mail_plan, number_vmail_messages=number_vmail_messages, total_day_minutes=total_day_minutes, total_day_calls=total_day_calls, total_day_charge=total_day_charge, total_eve_minutes=total_eve_minutes, total_eve_calls=total_eve_calls, total_eve_charge=total_eve_charge, total_night_minutes=total_night_minutes, total_night_calls=total_night_calls, total_night_charge=total_night_charge, total_intl_minutes=total_intl_minutes, total_intl_calls=total_intl_calls, total_intl_charge=total_intl_charge, customer_service_calls=customer_service_calls, predictions=predictions, churn_result=churn_result, churn_result_percentage=churn_result_percentage)
-
 
 
 if __name__ == '__main__':
