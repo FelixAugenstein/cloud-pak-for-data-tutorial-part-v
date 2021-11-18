6fed9960e6bb5a342a891e3e221c751f5d61b9dc
janhofmann01 <93585799+janhofmann01@users.noreply.github.com>
2021-11-18 13:42:07 +0100

    Update rules.py

--- prediction/rules.py
+++ prediction/rules.py
@@ -1,8 +1,8 @@
 # prediction rules for customer churn true or false
 def predictionRules(prediction_churn_true_or_false, prediction_churn_true_or_false_percentage_one, prediction_churn_true_or_false_percentage_two):
-    if prediction_churn_true_or_false == 0:
+    if prediction_churn_true_or_false == 0 or prediction_churn_true_or_false == "no" or prediction_churn_true_or_false == "False":
         predictionRules.churn_result = "This customer will not churn."
-    elif prediction_churn_true_or_false == 1:
+    elif prediction_churn_true_or_false == 1 or prediction_churn_true_or_false == "yes" or prediction_churn_true_or_false == "True":
         predictionRules.churn_result = "This customer will churn."
     else:
         predictionRules.churn_result = "There is no accurate prediction for this customer." 
